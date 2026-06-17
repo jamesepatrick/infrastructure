a629465c4a51e4a6db56db7a863491cd80ff3f4e
James Patrick <james@greenfiling.com>
2026-06-17 12:50:14 -0400

    fix(s3_backend) explicity state defaults
    This was causing drift detection to go off even when no changes are
    applied.

--- modules/s3_backend/main.tf
+++ modules/s3_backend/main.tf
@@ -18,6 +18,9 @@ resource "aws_s3_bucket" "terraform_state" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
   rule {
+    blocked_encryption_types = ["SSE-C"]
+    bucket_key_enabled       = false
+
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
